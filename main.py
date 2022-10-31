e62d79779a7dd77d250f088b97eaace1631edf66
anastya_dru <adruts@mail.ru>
2022-11-01 00:34:57 +0300

    First commit

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import requests
 
 response = requests.get("https://httpbin.org/get")
 
+headers =
+
 #if response.statuc_code == 200:
     #print("OK")
 
-#response.raise_for_status()
-
-response =
+response.raise_for_status()
 
 print(response)
