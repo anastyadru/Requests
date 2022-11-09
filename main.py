107dcd2bfa4b0ac21eccac7d82aea6d5ef54598d
anastya_dru <adruts@mail.ru>
2022-11-10 01:57:56 +0300

    First commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ headers = {
     "User-Agent": "IT OVERONE"
 }
 
-response = requests.get("https://httpbin.org/get", headers=headers)
+response = requests.get("https://httpbin.org/get?a=b", headers=headers)
 
 
 print(response.text)
