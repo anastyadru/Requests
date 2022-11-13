c8e2c2032697c5382c9f284bb82487f5365683db
anastya_dru <adruts@mail.ru>
2022-11-14 00:03:41 +0300

    First commit

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ headers = {
 response = requests.post("https://httpbin.org/post",
                          headers=headers,
                          params={'a': 'b'},
-
+                         json={})
 
 
 print(response.text)
