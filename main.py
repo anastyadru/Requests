94a18ade07137b83a24bdf9237dab19c95d60644
anastya_dru <adruts@mail.ru>
2022-11-19 15:40:56 +0300

    First commit

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
 import requests
 
-headers = {
-    "User-Agent": "IT OVERONE"
-}
-
-response = requests.post("https://httpbin.org/post",
-                         headers=headers,
-                         params={'a': 'b'},
-                         json={'username': 'supersecret'})
-
-
-print(response.text)
+website =
