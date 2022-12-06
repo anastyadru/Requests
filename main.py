aa79e18f6087123f188e54914eb0ee8a63b05e8a
anastya_dru <adruts@mail.ru>
2022-12-06 13:30:26 +0300

    First commit

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ website = "https://jsonplaceholder.typicode.com/posts"
 response = requests.post(website, json={
     "userld": 12,
     "title": "my new post",
-    "body": "body for my post IT"
+    "body": "body for my post IT",
+    "photo":
 })
 
 print(response.text)
