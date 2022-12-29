5983a8623ad23b826662795842ca420a85ee94d1
anastya_dru <adruts@mail.ru>
2022-12-29 11:35:28 +0300

    First commit

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import requests
 
 website = "https://jsonplaceholder.typicode.com/posts/1"
-print(requests.get(website).json())
+print(requests)
 
 response = requests.delete(website, data={
     "id": 1,
     "userld": 12,
     "title": "my new post",
-})
+})`
